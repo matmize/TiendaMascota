3f8039794963b6750cf140faf3a8a34e98478d32
Mateo Montoya <mateomontoya@Mateos-MacBook-Pro.local>
2021-03-20 10:40:09 -0500

    make it better

--- src/main/java/servlet/Unico.java
+++ src/main/java/servlet/Unico.java
@@ -1,23 +1,18 @@
 package servlet;
 
-public class Unico {
-	private int contador = 0;
-	private static Unico unico = null;
-	private var Empleado = [];
-	private var Proyecto = [];
-	private var Asignacion = [];
+public class Unico{
+    private int contador = 0;
+    private static Unico unico = null;
 
-
-	public Unico(){
-	}
-	public static Unico crear(){
-		if (unico == null )
-				unico = new Unico ();
-		return unico;
-	}
-
-	public int getContador(){
-		contador ++;
-		return contador;
-	}
+    public Unico(){
+    }
+    public static Unico crear(){
+        if (unico == null);
+            unico = new Unico();
+        return unico;
+    }
+    public int getContador(){
+        contador = ++;
+        return contador;
+    }
 }

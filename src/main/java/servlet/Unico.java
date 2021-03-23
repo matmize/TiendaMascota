c8557376402a1e78ab72465983623ec8d22885a0
Mateo Montoya <mateomontoya@Mateos-MacBook-Pro.local>
2021-03-23 18:25:38 -0500

    make it better

--- src/main/java/servlet/Unico.java
+++ src/main/java/servlet/Unico.java
@@ -2,16 +2,26 @@ package servlet;
 
 import java.util.*;
 
+
+
 public class Unico{
     private static Unico INSTANCE = null;
     private ArrayList<Empleado>empleados = new ArrayList();
     private ArrayList<Proyecto>proyectos = new ArrayList();
     private ArrayList<Asignacion>asignaciones = new ArrayList();
 
+    public class Unico getInstance (){
+        <%@page import = "servlet.*" %>
+        <%@page import = "Empleado" %>
+        <%@page import = "Proyecto" %>
+        <%@page import = "Asignacion" %>
+    }
+
 
     public void crearEmpleado (int id, String nombre, String habilidades){
         Empledo empleado = new Empledo (id, nombre, habilidades);
         empleados.add(empleado);
     }
+
     
 }

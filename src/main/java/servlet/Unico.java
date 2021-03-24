4d73531b8e2c57676e8b759e295f737dcc011f11
Mateo Montoya <mateomontoya@Mateos-MacBook-Pro.local>
2021-03-24 15:13:22 -0500

    make it better

--- src/main/java/servlet/Unico.java
+++ src/main/java/servlet/Unico.java
@@ -166,11 +166,11 @@ public class Unico{
             }else{
 
 
-                Asignacion asignacion = new Asignacion (-100,0,0,0,"-")
+                Asignacion asignacion = new Asignacion (-100,0,0,0,"-");
                 return asignacion;
             }
         }else{
-           Asignacion asignacion = new Asignacion (-100,0,0,0,"-")
+           Asignacion asignacion = new Asignacion (-100,0,0,0,"-");
             return asignacion;
         }
 

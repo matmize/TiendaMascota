3454d0cf2826a480db811f7eb80e8ac50e31a1fd
Mateo Montoya <mateomontoya@Mateos-MacBook-Pro.local>
2021-04-12 10:58:55 -0500

    make it better

---
+++ src/main/java/servlet/crearProducto.java
@@ -0,0 +1,54 @@
+package servlet;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.Math;
+
+@WebServlet(
+    name = "myServel",
+    urlPatterns = {"/crear"}
+)
+
+public class crearProducto extends HttpServlet {
+    @Override
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+
+        Unico unico = Unico.getInstance();
+
+        int codigo = Integer.parseInt(req.getParameter("codigo"));
+        String nombre = req.getParameter("nombre");
+        String descripcion = req.getParameter("descripcion");
+        int valorUni = Integer.parseInt(req.getParameter("valorUni"));
+        int cantidad = Integer.parseInt(req.getParameter("cantidad"));
+        ServletOutputStream out;
+
+        String rpta = "ya existe un producto con el codigo " + codigo;
+        String rpta1 = "Se ha creado un producto con el codigo" + codigo;
+
+        if (unico.buscarProducto(codigo).getCodigo() == -100){
+            unico.crearProducto(codigo,nombre, descripcion, valorUni,cantidad);
+            ServletOutputStream out = resp.getOutputStream();
+            out.write(rpta1.getBytes());
+
+            
+
+        }else{
+            ServletOutputStream out = resp.getOutputStream();
+            out.write(rpta.getBytes());
+
+            
+        }
+
+        out.flush();
+        out.close();
+    }
+}
+
+
+    
